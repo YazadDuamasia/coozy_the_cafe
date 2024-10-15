ceb51c9df3bce5669ed2a0ef12e5b2b5a18090a4
YazadDumasia <yazad14015dumasia+github@gmail.com>
2024-10-15 23:09:21 +0530

    image update

--- android/app/src/main/kotlin/com/app/coozy_the_cafe/MainActivity.kt
+++ android/app/src/main/kotlin/com/app/coozy_the_cafe/MainActivity.kt
@@ -1,5 +1,12 @@
 package com.app.coozy_the_cafe
 
+import android.view.WindowManager.LayoutParams
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity(){
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine){
+        super.configureFlutterEngine(flutterEngine)
+        window.addFlags(LayoutParams.FLAG_SECURE)
+    }
+}
